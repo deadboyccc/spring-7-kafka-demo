f07522e86b8c30459aac9992b8207228c9e1e5b6
Ahmed M. Ali <ahmedmohammed199975@gmail.com>
2026-02-05 14:21:22 +0300

    initialize Spring Boot project with Maven and Kafka dependencies

---
+++ src/main/java/dev/dead/spring7kafkademo/Spring7KafkaDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.dead.spring7kafkademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring7KafkaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring7KafkaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/dead/spring7kafkademo/Spring7KafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dead.spring7kafkademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring7KafkaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
